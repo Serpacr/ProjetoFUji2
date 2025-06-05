7477dfbd3ebe56507234a443d8598c92df5c6a48
Serpacr <164097767+Serpacr@users.noreply.github.com>
2025-06-05 18:38:00 -0300

    Update TarefaRepository.java

--- ProjetoFuji-main/ProjetoFuji-main/todo-api-intellij (1)/src/main/java/com/exemplo/todo/repository/TarefaRepository.java
+++ ProjetoFuji-main/ProjetoFuji-main/todo-api-intellij (1)/src/main/java/com/exemplo/todo/repository/TarefaRepository.java
@@ -2,5 +2,15 @@ package com.exemplo.todo.repository;
 
 import com.exemplo.todo.model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {}
+import java.util.List;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+    @Query("SELECT t FROM Tarefa t WHERE t.concluida = true")
+    List<Tarefa> buscarTarefasConcluidas();
+
+}
